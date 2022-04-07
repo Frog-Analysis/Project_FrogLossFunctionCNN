0bf188b4562d44ddc773442a25fe6e5272d2bff9
Jerry-Jie-Xie <xiej8734@gmail.com>
2022-04-07 20:41:08 +0800

    update

--- Project_FrogLossFunctionCNN_Brazil/Brazil_1D_2D_frog_clean_org_v1.py
+++ Project_FrogLossFunctionCNN_Brazil/Brazil_1D_2D_frog_clean_org_v1.py
@@ -273,9 +273,10 @@ if __name__ == '__main__':
                     training_feat_waveform = train_feat_waveform[training_index,:]
                     validation_feat_waveform = train_feat_waveform[validation_index,:]
                     
-                    training_feat_waveform = StandardScaler(copy=False).fit_transform(training_feat_waveform)
-                    validation_feat_waveform = StandardScaler(copy=False).fit_transform(validation_feat_waveform)
-                    testFeature_waveform = StandardScaler(copy=False).fit_transform(test_feat_waveform)
+                    std_scaler = StandardScaler(copy=False).fit(training_feat_waveform)
+                    training_feat_waveform = std_scaler.transform(training_feat_waveform)
+                    validation_feat_waveform = std_scaler.transform(validation_feat_waveform)
+                    testFeature_waveform = std_scaler.transform(test_feat_waveform)
                     
                     #===========================#
                     print('reshape train data')
